458f6c16d0002bcefb2fefb0a8fe65948ec472ca
mikaeletha <mikaelethainara@gmail.com>
2024-09-29 21:03:29 +0100

    salva csv com dados tratados

--- duplicado.py
+++ duplicado.py
@@ -6,57 +6,75 @@ porta_serial = 'COM3'  # Substitua pelo nome da porta serial do seu dispositivo
 baud_rate = 9600
 timeout = 1
 
-nome_arquivo = 'dados_luz_duplicado.csv'
-
 ser = serial.Serial(porta_serial, baud_rate, timeout=timeout)
 
-with open(nome_arquivo, 'w', newline='') as csvfile:
-    fieldnames = ['Index', 'DataHora', 'Luz (lx)', 'Observacao']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+try:
+    index = 0
+    observacao = ""
+    dados_duplicados = []
 
-    writer.writeheader()
+    while True:
+        linha = ser.readline().decode('utf-8').strip()
 
-    try:
-        index = 0
-        observacao = ""
-        dados_duplicados = []
+        if linha:
+            luz = str(linha).replace('sensor =', '').strip()
+            luz = float(luz)
+            data_hora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-        while True:
-            linha = ser.readline().decode('utf-8').strip()
+            dados = {
+                'Index': index,
+                'DataHora': data_hora,
+                'Luz (lx)': str(luz).replace('.', ','),
+                'Observacao': observacao
+            }
 
-            if linha:
-                luz = str(linha).replace('sensor =', '').strip()
-                luz = float(luz)
-                data_hora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            # Se múltiplo de 3, duplicar a linha
+            if index % 3 == 0 and index != 0:
+                dados_duplicados.append(dados.copy())
+                dados['Observacao'] = "Esse valor foi duplicado"
+                dados_duplicados.append(dados.copy())
 
-                dados = {
-                    'Index': index,
-                    'DataHora': data_hora,
-                    'Luz (lx)': str(luz).replace('.', ','),
-                    'Observacao': observacao
-                }
+            else:
+                dados_duplicados.append(dados)
 
-                # Se múltiplo de 3, duplicar a linha
-                if index % 3 == 0 and index != 0:
-                    dados_duplicados.append(dados.copy())
-                    dados['Observacao'] = "Esse valor foi duplicado"
-                    dados_duplicados.append(dados.copy())
+            print(f"Index: {index}, Data: {data_hora}, Luz: {
+                luz} lx")
 
-                else:
-                    dados_duplicados.append(dados)
+            index += 1
 
-                print(f"Index: {index}, Data: {data_hora}, Luz: {
-                      luz} lx")
+except KeyboardInterrupt:
+    print("Interrupção manual pelo usuário.")
+finally:
+    ser.close()
+    print("Conexão serial fechada.")
 
-                index += 1
+    print("\nDados duplicados:")
+    for item in dados_duplicados:
+        print(f"Index: {item['Index']}, Data: {item['DataHora']}, Luz: {
+            item['Luz (lx)']}, Observação: {item['Observacao']}")
 
-    except KeyboardInterrupt:
-        print("Interrupção manual pelo usuário.")
-    finally:
-        ser.close()
-        print("Conexão serial fechada.")
+    # Remover duplicados
+    dados_filtrados = []
+    indices_vistos = set()
 
-        print("\nDados duplicados:")
-        for item in dados_duplicados:
-            print(f"Index: {item['Index']}, Data: {item['DataHora']}, Luz: {
-                  item['Luz (lx)']}, Observação: {item['Observacao']}")
+    for item in dados_duplicados:
+        if item['Index'] not in indices_vistos:
+            dados_filtrados.append(item)
+            indices_vistos.add(item['Index'])
+
+    # Exibe os dados após remoção dos duplicados
+    # print("\nDados após remoção de duplicados:")
+    # for item in dados_filtrados:
+    #     print(f"Index: {item['Index']}, Data: {item['DataHora']}, Luz: {
+    #           item['Luz (lx)']}")
+
+    # Salva no csv os dados tratados
+    nome_arquivo = 'dados_luz_duplicado.csv'
+    with open(nome_arquivo, 'w', newline='') as csvfile:
+        fieldnames = ['Index', 'DataHora', 'Luz (lx)', 'Observacao']
+        writer = csv.DictWriter(
+            csvfile, fieldnames=fieldnames, delimiter=';')
+        writer.writeheader()
+
+        for item in dados_filtrados:
+            writer.writerow(item)
